ee068e40584b93cdef671643f9048d2e96859707
Azzariyat <166045091+Azzariyat@users.noreply.github.com>
2025-02-25 22:37:34 +0700

    Update solution.js

--- solution.js
+++ solution.js
@@ -113,24 +113,6 @@ function bulatkan(a) {
 
 // Export hasil akhir
 
-/**
- * Mengembalikan nilai terkecil dari dua angka.
- * @param {number} a 
- * @param {number} b 
- * @returns {number}
- */
-function minimum(a, b) {
-    return Math.min(a, b);
-}
-
-/**
- * Mengembalikan hasil dari a dibulatkan ke bilangan bulat terdekat.
- * @param {number} a 
- * @returns {number}
- */
-function bulatkan(a) {
-    return Math.round(a);
-}
 
 // Export semua fungsi yang tersedia
 module.exports = {
